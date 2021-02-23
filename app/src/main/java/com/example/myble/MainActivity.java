03d9ab8d4a442333e18cf82a3bde20fa00e03c12
eunjin kim <eunjin@ninewatt.com>
2021-02-23 16:11:00 +0900

    1. BluetoothAdapter(블루투스 송수신 장치) 가져오기  ble가 지원되지만 비활성화인 경우 사용자가 앱을 떠나지 않은 상태에서 ble를 활성화 하도록 요청(BluetoothAdapter 사용)  * 모든 블루투스 Activity에는 BluetoothAdapter가 필요

--- app/src/main/java/com/example/myble/MainActivity.java
+++ app/src/main/java/com/example/myble/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 0. 저전력 블루투스의 가용성을 지정, 만약 ble 지원이 안된다면 ble기능을 비활성화 해야함
-        // 매니페스트에 android.hardware.bluetooth_le를 false 해두면 ble 사용가능 여부 확인하는게 좋다
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
         }
 
+        // 1. BluetoothAdapter(블루투스 송수신 장치) 가져오기
+        //  getSystemService()를 사용하여 BluetoothManager의 인스턴스를 반환한 다음, 어댑터를 가져옴
+       final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
 
 
     }
